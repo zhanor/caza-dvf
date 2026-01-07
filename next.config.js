f97d24dd93f6145e9db8b173983753c1c4bebd03
zhanor <zatecka.m@gmail.com>
2026-01-07 15:20:24 +0100

    Fix: rewrite db and auth logic

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // 1. On empêche Next.js de compiler ces librairies (Server Components)
   experimental: {
     serverComponentsExternalPackages: ['bcryptjs', 'pg'],
+  },
+  // 2. On force Webpack à les ignorer totalement (Méthode radicale)
+  webpack: (config) => {
+    config.externals = [...(config.externals || []), 'bcryptjs', 'pg'];
+    return config;
   },
 };
 
 module.exports = nextConfig;
-
